f979213b81393f33d4500ad329a7243ca7774808
Ryan LaBouve <imryanlabouve@gmail.com>
2017-06-04 20:16:08 -0500

    Some feedback when gets attacked

--- app/components/battle-scene/component.js
+++ app/components/battle-scene/component.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: '',
+  _cachedHealthPercentage: null,
+
   healthPercentage: Ember.computed('battle.hp_total', 'battle.damage_total', function() {
     let hp = Number.parseInt(this.get('battle.hp_total'));
     let damage = Number.parseInt(this.get('battle.damage_total'));
@@ -14,4 +16,20 @@ export default Ember.Component.extend({
   threeForthsDead: Ember.computed.lt('healthPercentage', 26),
   almostDead: Ember.computed.lt('healthPercentage', 11),
   dead: Ember.computed.lt('healthPercentage', 1),
+
+
+  recentlyAttacked: Ember.computed('healthPercentage', function() {
+    if (!this.get('_cachedHealthPercentage')) {
+      this.set('_cachedHealthPercentage', this.get('healthPercentage'));
+      return false;
+    }
+
+    if (this.get('_cachedHealthPercentage') !== this.get('healthPercentage')) {
+      // side-effect
+      this.set('_cachedHealthPercentage', this.get('healthPercentage'));
+      return true;
+    } else {
+      return false;
+    }
+  })
 });
