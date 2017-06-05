1131c50df81c052550945d41703ea498291126cc
Ryan LaBouve <imryanlabouve@gmail.com>
2017-06-04 19:14:07 -0500

    Progressive death of enemy

--- app/components/battle-scene/component.js
+++ app/components/battle-scene/component.js
@@ -5,7 +5,13 @@ export default Ember.Component.extend({
   healthPercentage: Ember.computed('battle.hp_total', 'battle.damage_total', function() {
     let hp = Number.parseInt(this.get('battle.hp_total'));
     let damage = Number.parseInt(this.get('battle.damage_total'));
-    let percentage = (damage / hp) * 100;
-    return `${percentage}%`;
-  }).readOnly()
+    let percentage = ((hp -damage) / hp) * 100;
+    return percentage;
+  }).readOnly(),
+
+  oneForthsDead: Ember.computed.lt('healthPercentage', 76),
+  halfDead: Ember.computed.lt('healthPercentage', 51),
+  threeForthsDead: Ember.computed.lt('healthPercentage', 26),
+  almostDead: Ember.computed.lt('healthPercentage', 11),
+  dead: Ember.computed.lt('healthPercentage', 1),
 });
